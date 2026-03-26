f1709f518abd6c2489d4bd6fa2cbf259a65c9f66
LucasHyder <lucasjuzahyder@gmail.com>
2026-03-26 18:02:25 -0300

    Update Babel config to include Hermes stable profile

--- Babel.config.js
+++ Babel.config.js
@@ -1,3 +1,7 @@
 module.exports = {
-  presets: ['@react-native/babel-preset'],
+  presets: [
+    ['@react-native/babel-preset', {
+      unstable_transformProfile: 'hermes-stable',
+    }],
+  ],
 };
