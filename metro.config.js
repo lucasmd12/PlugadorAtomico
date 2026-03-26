696eb4b06073902fc1a18e0801f7a1cfee61361b
LucasHyder <lucasjuzahyder@gmail.com>
2026-03-26 18:00:55 -0300

    Update Metro configuration to include Babel transformer

--- metro.config.js
+++ metro.config.js
@@ -1,9 +1,7 @@
 const {getDefaultConfig, mergeConfig} = require('@react-native/metro-config');
 
-/**
- * Metro configuration
- * https://reactnative.dev/docs/metro
- */
-const config = {};
-
-module.exports = mergeConfig(getDefaultConfig(__dirname), config);
+module.exports = mergeConfig(getDefaultConfig(__dirname), {
+  transformer: {
+    babelTransformerPath: require.resolve('metro-react-native-babel-transformer'),
+  },
+});
